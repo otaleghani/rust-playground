e4adba61ebc0bdedf168f224caef16fd5eaa4809
Oliviero Taleghani <o.taleghani@talesign.com>
2024-08-19 17:21:51 +0200

    added: server

---
+++ basics/ch_20_web_server/server/src/main.rs
@@ -0,0 +1,54 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();    
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    // gets the hole request
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    // println!("{:#?}", http_request);
+    
+    let request_line = buf_reader
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap();
+    println!("{request_line}");
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let length = content.len();
+
+    let response = format!(
+        "{status_line}\r\n\
+        Content-length: {length}\r\n\r\n\
+        {content}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
