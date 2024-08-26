219f7a0489067e2194fd2f0d5ae5253f614e5917
Oliviero Taleghani <o.taleghani@talesign.com>
2024-08-26 17:31:47 +0200

    added: tildes

--- projects/text-editor/src/editor.rs
+++ projects/text-editor/src/editor.rs
@@ -1,5 +1,8 @@
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use std::io::stdout;
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode, Clear, ClearType, size};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::cursor::MoveTo;
 
 pub struct Editor {
     should_quit: bool,
@@ -11,32 +14,72 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            panic!("err:#?");
+        Self::initialize().unwrap();
+        Self::draw_rows();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error>{
+        loop {
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                _ => (),
+            }
         }
     }
 
-    pub fn repl(&mut self) -> Result<(), std::io::Error>{
-        print!("\x1b[2J");
-        enable_raw_mode()?;
-        print!("\x1b[2J");
-        loop {
-            if let Key(KeyEvent {
-                code, modifiers, kind, state
-            }) = read()? {
-                println!("Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?}, State: {state:?}\r");
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    },
-                    _ => ()
-                }
-                if self.should_quit {
-                    break;
-                }
-            }
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
         }
-        disable_raw_mode()?;
         Ok(())
+    }
+
+    fn draw_rows() {
+        let screen_size = size();
+
+        match screen_size {
+            Ok((x, y)) => {
+                for i in 0..y {
+                    execute!(stdout(), MoveTo(0, i));
+                    print!("~");
+                }
+            },
+            Err(err) => ()
+        }
     }
 }
