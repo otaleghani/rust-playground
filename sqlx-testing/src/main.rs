d0f2a933d22e8e0d0d0b481b8734742dc8c91ced
Oliviero Taleghani <o.taleghani@talesign.com>
2025-02-05 11:46:58 +0000

    added: sql current open connections

--- sqlx-testing/src/main.rs
+++ sqlx-testing/src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{Pool, Sqlite, SqlitePool};
+use sqlx::{Pool, Row, Sqlite, SqlitePool};
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 
@@ -11,8 +11,6 @@ struct Database {
     pool: Pool<Sqlite>,
     schema: Arc<RwLock<DatabaseSchema>>,
 }
-
-// type Connections = Arc<RwLock<HashMap<String, Pool<Sqlite>>>>;
 
 #[derive(Clone)]
 struct State {
@@ -44,7 +42,7 @@ async fn main() -> Result<(), sqlx::Error> {
     let second_db = "second.db";
     let pool = create_database(second_db, state.clone()).await?;
     state.projects.write().unwrap().insert(
-        first_db.to_string(),
+        second_db.to_string(),
         Database {
             pool,
             schema: Arc::new(RwLock::new(HashMap::new())),
@@ -62,18 +60,27 @@ async fn main() -> Result<(), sqlx::Error> {
         },
     ];
 
-    add_table(state.clone(), "first.db", fields, "new_table").await?;
+    add_table(state.clone(), "second.db", fields.clone(), "new_table").await?;
+    add_table(state.clone(), "first.db", fields.clone(), "new_table").await?;
+    load_databases(state.clone()).await?;
 
     Ok(())
 }
 
 // Create a new database based on name
 async fn create_database(filename: &str, state: State) -> Result<Pool<Sqlite>, sqlx::Error> {
-    tokio::fs::File::create(&filename).await.unwrap();
+    let _ = tokio::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true) // Appends instead of truncating
+        .open(filename)
+        .await
+        .unwrap();
+    // tokio::fs::File::create(&filename).await.unwrap();
     let pool = SqlitePool::connect(&filename).await?;
 
     // Inserts the database filename inside of the master database
-    let query = "INSERT INTO databases (id, path) VALUES (?, ?)";
+    let query = "INSERT OR IGNORE INTO databases (id, path) VALUES (?, ?)";
 
     let master_pool = state.master;
 
@@ -87,7 +94,14 @@ async fn create_database(filename: &str, state: State) -> Result<Pool<Sqlite>, s
 }
 
 async fn open_master_database() -> Result<Pool<Sqlite>, sqlx::Error> {
-    tokio::fs::File::create(&MASTER).await.unwrap();
+    let _ = tokio::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true) // Appends instead of truncating
+        .open(&MASTER)
+        .await
+        .unwrap();
+    // tokio::fs::File::create(&MASTER).await.unwrap(); // This one will truncate the content to 0 lenght,
     let db = SqlitePool::connect(&MASTER).await?;
     let query = "
     CREATE TABLE IF NOT EXISTS databases (
@@ -107,6 +121,7 @@ async fn open_master_database() -> Result<Pool<Sqlite>, sqlx::Error> {
 pub enum FieldType {
     String,
     Int,
+    Sus,
 }
 
 #[derive(Clone)]
@@ -128,6 +143,7 @@ async fn add_table(
         match field.field_type {
             FieldType::String => query = format!("{} {} TEXT", query, field.name),
             FieldType::Int => query = format!("{} {} INTEGER", query, field.name),
+            FieldType::Sus => query = format!("{} {} TEXT", query, field.name),
         }
         if index != fields.len() - 1 {
             query = format!("{},", query);
@@ -148,26 +164,135 @@ async fn add_table(
     Ok(())
 }
 
+#[derive(sqlx::FromRow)]
 struct Datab {
     id: String,
     path: String,
 }
-async fn load_databases(state: State) -> Result<(), sqlx::Error> {
-    // TODO: Check if there is a master database
-    let master_pool = state.master;
-    let database_info_query = "PRAGMA table_list";
-    let table_info_query = "PRAGMA table_info('{}')";
 
+#[derive(sqlx::FromRow)]
+struct Db {
+    name: String,
+}
+
+#[derive(Debug, sqlx::FromRow)]
+struct Table {
+    name: String,
+    r#type: String,
+}
+
+async fn load_databases(state: State) -> Result<(), sqlx::Error> {
+    // Check if there is a master database
+    println!("==> Starting loading...");
+    let master_pool = state.master.clone();
+
+    // Prepare the queries
     let databases_query = "SELECT * FROM databases";
 
-    // let result = sqlx::query(databases_query).execute(&master_pool).await?;
-    let result = sqlx::query_as!(Datab, "SELECT * FROM databases")
+    // Check databases in master database
+    let databases = sqlx::query_as::<_, Datab>(databases_query)
         .fetch_all(&master_pool)
         .await?;
 
-    // TODO: Check databases in master database
-    // TODO: Foreach database, get the different tables
-    // TODO: Foreach table, get the different columns
+    // Foreach database, get the different tables
+    for db in &databases {
+        println!("==> Database {}", db.id);
 
+        // Create pools
+        let pool = create_database(&db.path, state.clone()).await?;
+        state.projects.write().unwrap().insert(
+            db.path.to_string(),
+            Database {
+                pool: pool.clone(),
+                schema: Arc::new(RwLock::new(HashMap::new())),
+            },
+        );
+        println!("==> Database pool opened");
+
+        // Get table names
+        let tables =
+            sqlx::query_as::<_, Db>("SELECT name FROM pragma_table_list WHERE type = 'table'")
+                .fetch_all(&pool)
+                .await?;
+
+        for tb in &tables {
+            println!("==> Table {:>4}", tb.name);
+
+            // Foreach table, get the different columns
+            let table_info_query =
+                format!("SELECT name, type FROM pragma_table_info('{}')", tb.name);
+            let t = sqlx::query_as::<_, Table>(&table_info_query)
+                .fetch_all(&pool)
+                .await?;
+
+            println!("{:?}", t);
+
+            // Create the correct HashMap to store the different columns inside
+            for values in &t {
+                println!("==> Column {:8}", values.name);
+
+                let data = state.projects.write().unwrap();
+                let databasa = data.get(&db.path.to_string()).unwrap();
+                let mut schema = databasa.schema.write().unwrap();
+                schema.insert(
+                    values.name.clone(),
+                    vec![Field {
+                        name: values.name.clone(),
+                        field_type: match values.r#type.as_str() {
+                            "TEXT" => FieldType::String,
+                            "INTEGER" => FieldType::Int,
+                            _ => FieldType::Sus,
+                        },
+                    }],
+                );
+            }
+        }
+        println!("");
+    }
+
+    Ok(())
+}
+
+// TODO: Add a dummy data to a table using JSON
+async fn print_to_db(
+    database: String,
+    table: String,
+    state: State,
+    data: HashMap<String, serde_json::Value>,
+) -> Result<(), sqlx::Error> {
+    // The table structure should already be in the state
+    // Get database
+    let projects = state.projects.read().unwrap();
+    let project_database = projects.get(&database.to_string()).unwrap();
+    let pool = project_database.pool.clone();
+
+    let schema = sqlx::query("PRAGMA table_info(users)")
+        .fetch_all(&pool)
+        .await?;
+
+    for row in &schema {
+        let sus: i32 = row.get("sandro");
+    }
+
+    // let valid_columns: Vec<String> = schema.iter().map(|row| row.get("sus")).collect();
+
+    // for key in data.keys() {
+    //     if !valid_columns.contains(key) {
+    //         eprintln!("Invalid column: {}", key);
+    //         return Ok(()); // Exit early if an invalid column is found
+    //     }
+    // }
+
+    // Now we can run the SQL -> We kinda want to understand if said
+    //
+
+    // TODO: Get structure
+
+    // TODO: Build query
+
+    Ok(())
+}
+// TODO: Read data from a table by a string
+async fn read_from_db(database: String, table: String, state: State) -> Result<(), sqlx::Error> {
     Ok(())
 }
